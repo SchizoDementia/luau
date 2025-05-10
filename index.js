01b2a1178aae5181eb9a6f23e6cec0ab637ac7e9
I have Dementia and Schizophrenia <lightgray2.v2@gmail.com>
2025-05-11 06:34:30 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3000;
+
+let scripts = {};
+try {
+  const data = fs.readFileSync(path.join(__dirname, 'index.json'), 'utf8');
+  scripts = JSON.parse(data);
+} catch (err) {
+  console.error("Failed to load index.json:", err);
+}
+
+function isBrowser(req) {
+  const accept = req.headers.accept || '';
+  const ua = req.headers['user-agent'] || '';
+  return accept.includes('text/html') || /mozilla|chrome|safari|edge|firefox/i.test(ua);
+}
+
+app.use(express.static(__dirname));
+
+app.get('/script/api/:scriptName', (req, res) => {
+  const id = req.params.scriptName?.toLowerCase();
+  const script = scripts[id];
+
+  if (script) {
+    if (isBrowser(req)) {
+      return res.sendFile(path.join(__dirname, 'index.html'));
+    } else {
+      res.type('text/plain');
+      return res.send(script);
+    }
+  } else {
+    return res.sendFile(path.join(__dirname, 'index1.html'));
+  }
+});
+
+app.get('/script/api', (req, res) => {
+  return res.sendFile(path.join(__dirname, 'index1.html'));
+});
+
+app.get('/', (req, res) => {
+  return res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('*', (req, res) => {
+  return res.sendFile(path.join(__dirname, 'index1.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
